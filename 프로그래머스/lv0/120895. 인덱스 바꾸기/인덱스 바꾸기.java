77416d55f46bc4c0d7f32639b961207c7cc46f67
haeseon lee <haeseon4816@gmail.com>
2023-04-06 15:12:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        StringBuilder sb = new StringBuilder(my_string);
+        char tmp = sb.charAt(num1);
+        sb.setCharAt(num1,sb.charAt(num2));
+        sb.setCharAt(num2,tmp);
+        return sb.toString();
+    }
+}
