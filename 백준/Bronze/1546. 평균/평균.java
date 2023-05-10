e3cccbc08ec5107f90e221cde889e78066d7a18a
haeseon lee <haeseon4816@gmail.com>
2023-05-10 10:34:29 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 20252 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,18 @@
-import java.util.Scanner;
-public class Main {
+import java.util.*;
+public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        
-        for(int i = 0; i<n; i++){
-            a[i] = sc.nextInt();
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i = 0; i < N; i++){
+            A[i] = sc.nextInt();
         }
-        long sum = 0;
-        long max = 0;
-        
-        for(int i = 0; i<n; i++){
-            if(a[i] > max) max = a[i];
-            sum = sum + a[i];
+        int max = A[0];
+        int sum = 0;
+        for(int i = 0; i < A.length; i++){
+            if(A[i] > max) max = A[i];
+            sum = sum + A[i]; 
         }
-        System.out.println(sum * 100.0/max/n);
+        System.out.println(sum * 100.0 / max / N);
     }
 }
